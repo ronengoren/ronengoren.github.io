98b5072dc4ede2a330aa1eb08d1d01ea8a9a8ab7
tkelly16 <121046255+tkelly16@users.noreply.github.com>
2022-12-19 13:19:02 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,2 +1,3 @@
 // https://static.app.delivery/sdks/web/optimove-worker.js
-https://static.app.delivery/sdks/web/worker.js
+// https://static.app.delivery/sdks/web/worker.js
+importScripts('https://static.app.delivery/sdks/web/worker.js');
